ab6d65b5712e04af4f2bac6241794a5882cb2c75
Evan Dorn <evan.dorn@joinhoney.com>
2021-02-18 14:31:17 -0800

    Initial empty files and folders

---
+++ internal/initialize_server.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func initialize() {
+  /* launch websocket listener
+     to wait for incoming connections
+  */
+
+  /* create game objects */
+
+  /* start ECS and physics update loop */
+}
