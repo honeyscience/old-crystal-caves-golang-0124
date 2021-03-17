f90cb714efbb1185720b56c8890df5dfa39a690b
Evan Dorn <evan.dorn@joinhoney.com>
2021-03-17 12:43:00 -0700

    Add go.mod and wire up initializer files so they will run

--- internal/initializer/initializer.go
+++ internal/initializer/initializer.go
@@ -1,13 +1,15 @@
-package main
+package initializer
 
 import "fmt"
 
-func initialize() {
+func Initialize() {
+  fmt.Println("Running initializer")
+
+  /* create game objects */
+
   /* launch websocket listener
      to wait for incoming connections
   */
-
-  /* create game objects */
 
   /* start ECS and physics update loop */
 }
